33fc2477b9c67c44cee3ff88f8ffe1de88f28a30
bszymala <bszymala20@gmail.com>
2025-12-16 18:29:37 +0100

    Made the app display test chart

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
-import matplotlib
+from flask import Flask, render_template
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +14,15 @@ def helloworld():
 
 @app.route("/chart")
 def main():
-    return "Hello from chart"
+    x = [1,3,5]
+    y = [2,4,7]
+
+    plt.figure()
+    plt.plot(x, y)
+    os.makedirs("static", exist_ok=True)
+    plt.savefig("static/chart.png")
+    plt.close()
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
