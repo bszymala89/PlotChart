155a03384e796f0789fac38876c08ef42dd27cb3
bszymala <bszymala20@gmail.com>
2026-02-10 19:08:30 +0100

    removed all logic from main file

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 
-import matplotlib.pyplot as plt
-import mathUtils
+import plotUtils
 import numpy as np
-import os
 
 app = Flask(__name__)
 
@@ -11,21 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def helloworld():
     welcome_text = "Hello from server!"
-    # finalText = re.fullmatch(r"[x0-9 +-*/]", welcomeText.lower())
 
     return welcome_text
 
 
 @app.route("/chart", methods=["GET", "POST"])
 def main():
-    x = 1
-    y = 1
-
-    plt.figure()
-    plt.plot([x, y], [y, x])
-    os.makedirs("static", exist_ok=True)
-    plt.savefig("static/chart.png")
-    plt.close()
+    
+    plotUtils.draw_plot([0,0], "x")
 
     return render_template("index.html")
 
@@ -34,17 +25,7 @@ def main():
 def chart_post():
     equation = request.form.get("input")
 
-    x1 = 1
-    y1 = mathUtils.calculate_equation(equation, x1)
-
-    x2 = 4
-    y2 = mathUtils.calculate_equation(equation, x2)
-
-    plt.figure()
-    plt.plot([x1, x2], [y1, y2])
-    os.makedirs("static", exist_ok=True)
-    plt.savefig("static/chart.png")
-    plt.close()
+    plotUtils.draw_plot([1,4], equation)
 
     return render_template("index.html")
 
