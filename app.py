7f18ce7c17f2f1df0b476bac0c0b56159079b93d
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2026-03-02 18:22:54 +0100

    Added support to input multiple x (#4)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
 import plotUtils
+import mathUtils
 import numpy as np
 
 app = Flask(__name__)
@@ -24,9 +25,12 @@ def main():
 @app.route("/chart_post", methods=["GET", "POST"])
 def chart_post():
     equation = request.form.get("input")
+    min_x = request.form.get("min_x_input")
+    max_x = request.form.get("max_x_input")
+
     color = request.form.get("color_input")
 
-    plotUtils.draw_plot([1,4], equation, color)
+    plotUtils.draw_plot(mathUtils.convert_str_to_list(min_x, max_x), equation, color)
 
     return render_template("index.html")
 
@@ -34,3 +38,14 @@ def chart_post():
 if __name__ == "__main__":
     app.run()
 
+# 1. Przycisk do rysowania
+
+
+# 2, Zakres rysowania wykresu ( od jakiego x do jakiego x)
+# 3. Przycisk + zeby dodac kolejny wykres liniowy na charcie
+
+
+# 4. Zmiana koloru wykresu
+# 1 -> 4 -> 2 -> 3
+
+# https://getcssscan.com/css-buttons-examples
